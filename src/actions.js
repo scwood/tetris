4e6cdb68d3c7f5ec5b99532d3e415e371cb5eec8
Austin Wood <i@austinwood.me>
2017-06-07 23:55:10 -0700

    Include standard linter

--- src/actions.js
+++ src/actions.js
@@ -1,17 +1,17 @@
-const MOVE_DOWN = 'MOVE_DOWN';
-const MOVE_LEFT = 'MOVE_LEFT';
-const MOVE_RIGHT = 'MOVE_RIGHT';
+const MOVE_DOWN = 'MOVE_DOWN'
+const MOVE_LEFT = 'MOVE_LEFT'
+const MOVE_RIGHT = 'MOVE_RIGHT'
 
-function moveLeft() {
-  return { type: MOVE_LEFT };
+function moveLeft () {
+  return { type: MOVE_LEFT }
 }
 
-function moveRight() {
-  return { type: MOVE_RIGHT };
+function moveRight () {
+  return { type: MOVE_RIGHT }
 }
 
-function moveDown() {
-  return { type: MOVE_DOWN };
+function moveDown () {
+  return { type: MOVE_DOWN }
 }
 
 module.exports = {
@@ -20,5 +20,5 @@ module.exports = {
   MOVE_DOWN,
   moveLeft,
   moveRight,
-  moveDown,
+  moveDown
 }
