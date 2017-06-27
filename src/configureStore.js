cd297b6c051c6b614a9171aaba0ccdb6859589c2
Spencer Wood <spencercwood@gmail.com>
2017-06-27 02:13:58 -0600

    Add redux logger

--- src/configureStore.js
+++ src/configureStore.js
@@ -3,6 +3,7 @@ import grid from './grid'
 import score from './score'
 import tetromino from './tetromino'
 import thunk from 'redux-thunk'
+import logger from 'redux-logger'
 import { applyMiddleware, combineReducers, createStore } from 'redux'
 
 const rootReducer = combineReducers({
@@ -15,6 +16,6 @@ const rootReducer = combineReducers({
 export default function configureStore () {
   return createStore(
     rootReducer,
-    applyMiddleware(thunk)
+    applyMiddleware(thunk, logger)
   )
 }
